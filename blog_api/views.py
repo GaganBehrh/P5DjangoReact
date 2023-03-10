17e84d3a44224b67a9ea8001148c991c9383e146
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-03-10 14:18:42 +0000

    Post_Detail page visible and functional

--- blog_api/views.py
+++ blog_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
 from blog.models import Post
 from .serializers import PostSerializer
+
+
 class PostList(generics.ListCreateAPIView):
     queryset = Post.postobjects.all()
-    serializer_class=PostSerializer
+    serializer_class = PostSerializer
+
+
 class PostDetail(generics.RetrieveDestroyAPIView):
-    pass
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
