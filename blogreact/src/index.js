812d553d6b886fc692a36e186b3cc5ca9288f99c
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2023-03-12 15:53:07 +0000

    The Switch error fixed by the Route component

--- blogreact/src/index.js
+++ blogreact/src/index.js
@@ -1,25 +1,22 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 const routing = (
-  <Router>
-    <React.StrictMode>
-      <Header />
-
-      <Route exact path="/" component={App} />
-
-
-
-      <Footer />
-    </React.StrictMode>
-  </Router>
+	<Router>
+		<React.StrictMode>
+			<Header />
+			<Routes>
+				<Route exact path="/" component={App} />
+			</Routes>
+			<Footer />
+		</React.StrictMode>
+	</Router>
 );
 
 ReactDOM.render(routing, document.getElementById('root'));
